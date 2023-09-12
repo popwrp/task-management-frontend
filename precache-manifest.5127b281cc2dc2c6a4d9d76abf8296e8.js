a29b45f02e60ba5c1a296c40e54a28c37c3d20ad
popwrp <popwrp@users.noreply.github.com>
2023-09-12 12:43:57 +0000

    deploy: 7d555dcc5a9c766942e828173db65d4a72051497

---
+++ precache-manifest.5127b281cc2dc2c6a4d9d76abf8296e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d21ca3b5c3861582775c01d2bc39c55",
+    "url": "https://popwrp.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "1cb3bc5732be84f0404b",
+    "url": "https://popwrp.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "fd2f3c51c76cac2a4fbb",
+    "url": "https://popwrp.github.io/task-management-frontend/static/js/2.040a021b.chunk.js"
+  },
+  {
+    "revision": "1cb3bc5732be84f0404b",
+    "url": "https://popwrp.github.io/task-management-frontend/static/js/main.6c8f2690.chunk.js"
+  },
+  {
+    "revision": "86b6659c71410c1f2254",
+    "url": "https://popwrp.github.io/task-management-frontend/static/js/runtime~main.81a2939f.js"
+  }
+]);
